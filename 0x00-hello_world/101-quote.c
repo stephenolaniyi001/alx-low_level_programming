ce8cddaf346b29dbe2cb5d1d5f68a0544e8ca9c0
olokunde olaniyi <olokundeolaniyi@gmail.com>
2023-03-16 16:29:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
