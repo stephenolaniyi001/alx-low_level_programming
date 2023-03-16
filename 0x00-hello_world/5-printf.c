c00fcac1dae71c92f185d47785340ce9aa096d83
olokunde olaniyi <olokundeolaniyi@gmail.com>
2023-03-16 07:28:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print the phrase with proper grammar,
  * but the outcome is a piece of art,
@@ -7,6 +6,6 @@
  */
 int main (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
