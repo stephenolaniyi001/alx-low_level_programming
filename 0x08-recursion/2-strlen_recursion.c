31603ccebb34cc62868f96a2b151439cc98a18fb
olokunde olaniyi <olokundeolaniyi@gmail.com>
2023-04-06 09:12:38 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the lenght of a string
- * @s: The string to be measured
+ * _strlen_recursion - Returns the lenght of a string.
+ * @s: The string to be measured.
  *
- * Return: The lenght of the string
+ * Return: The lenght of the string.
  */
 int _strlen_recursion(char *s)
 {
-	int len = 0;
-
 	if (*s)
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
 	}
 }
